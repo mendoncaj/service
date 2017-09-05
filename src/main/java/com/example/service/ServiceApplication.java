35afae1f893068fce9e8766e66210f732077b661
willlobato <will.lobato@gmail.com>
2017-09-04 22:05:34 -0300

    primeiro commit

--- src/main/java/com/example/service/ServiceApplication.java
+++ src/main/java/com/example/service/ServiceApplication.java
@@ -4,31 +4,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.Sampler;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @SpringBootApplication
-//@EnableZipkinStreamServer
 public class ServiceApplication {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Bean
-	public Sampler defaultSampler() {
-		return new AlwaysSampler();
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 	@GetMapping("hi")
-	String hi(@RequestParam("param") String param) {
+	String hi(@RequestParam("param") String param, HttpServletRequest request) {
 
 		logger.info("chamada do service hi");
 
-		return "Hello World "+ param;
+		String message = restTemplate().getForEntity("http://localhost:8082/welcome", String.class).getBody();
+
+		return "Olá "+ param + ". "+ message;
 	}
 
 	public static void main(String[] args) {
